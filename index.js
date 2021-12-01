2a4355a9f79f2c2f19653fab14641b2a5a239976
eosfel97 <any.gouba@gmail.com>
2021-12-01 16:18:06 +0100

    avancée

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-alert("helo");
+let input = document.getElementsByTagName("span");
+let price = document.querySelectorAll(".price");
+let number = document.querySelectorAll(".number");
+let total = document.querySelectorAll(".total");
+// let totalg = document.querySelector(".totalg");
+
+// console.log(input);
+// console.log(price);
+// console.log(number);
+// console.log(totalg);
+for (let i = 0; i < input.length; i++) {
+  input[i].addEventListener("click", function () {
+    // console.log(number[i].value);
+    // console.log(price[i].textContent);
+    // console.log(total[i].textContent);
+    total[i].textContent = number[i].value * price[i].textContent + "€";
+    var totalg = 0;
+    $(".total").each(function () {
+      totalg += parseFloat($(this).text());
+    });
+    $(".totalg").html(totalg + "€");
+  });
+}
